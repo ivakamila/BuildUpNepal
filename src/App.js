01c20084cb200f72d238005f753950bcde3013f9
Kamila Ivashchenko <ivakamila@gmail.com>
2022-05-01 15:14:48 +0200

    fixed entrepreneur img margin

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.scss";
+import BrickTechnology from "./components/BrickTechnology/BrickTechnology";
 import EntrepreneurImg from "./components/EntrepreneurImg/EntrepreneurImg";
 import HeroSection from "./components/HeroSection/HeroSection";
 import KeyPartners from "./components/KeyPartners/KeyPartners";
@@ -9,6 +10,7 @@ function App() {
       <HeroSection />
       <KeyPartners />
       <EntrepreneurImg />
+      <BrickTechnology />
     </div>
   );
 }
---
+++ src/components/BrickTechnology/BrickTechnology.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const BrickTechnology = () => {
+  return (
+    <div className="technology">
+      <h2>Low-cost, eco friendly brick technology</h2>
+    </div>
+  );
+};
+
+export default BrickTechnology;
